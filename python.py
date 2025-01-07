ce2209cb1bf7ee5310cae4e56ed7edb93132f1a4
BallepalliVenkataBhavaniPrasad <prasadballepalli123@gmail.com>
2025-01-07 12:53:44 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+a=10
+a+=12
+print(a)
+a=10
+a-=12
+print(a)
+a=10
+a%=12
+print(a)
+a=10
+a*=12
+print(a)
+
+
+# In[6]:
+
+
+a=100
+b=200
+print(a+b)
+print(a-b)
+print(b/a)
+
+
+# In[7]:
+
+
+a=input("enter a number")
+b=input("enter a number")
+print(a+b)
+
+
+# In[8]:
+
+
+a=input("enter a number")
+b=input("enter a number")
+print(a+b)
+
+
+# In[9]:
+
+
+a=input("enter a number")
+b=input("enter a number")
+print(a+b)
+
+
+# In[13]:
+
+
+a=input("enter a string")
+b=input("enter a string")
+print(a+b)
+
+
+# In[15]:
+
+
+a=500
+b=1000
+c=1200
+print(a<b)
+print(b>c)
+print(a>c)
+
+
+# In[2]:
+
+
+a=300
+b=350
+c=400
+print(a>=b and c>a or c>b)
+print(b<c and b>a or c>=a)0.
+ 
+
+
+# In[4]:
+
+
+otp=int(input('enter otp'))
+if otp==2480:
+    print('correct')
+else:
+    print('enter a correct otp to start a bike ride')
+
+
+# In[2]:
+
+
+a="Prasad Ballepalli"
+a[0:6]
+
+
+# In[3]:
+
+
+a[10]
+
+
+# In[4]:
+
+
+a[0:12:5]
+
+
+# In[13]:
+
+
+a[0:1:-9]
+
+
+# In[11]:
+
+
+a[0:12:7]
+
+
+# In[19]:
+
+
+a[12:0:-4]
+
+
+# In[3]:
+
+
+ki=[1,2,3,4]
+ki1=[3,4,4,5]
+l1=zip(ki,ki1)
+print(l1)
+
+
+# In[4]:
+
+
+ki=[1,2,3,4]
+ki1=[3,4,4,5]
+l1=list(zip(ki,ki1))
+
+print("list of",l1)
+
+
+# In[ ]:
+
+
+
+
